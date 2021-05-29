56782bc29f6bca0832818ea7dc7c33d330270a37
Bowen Mao <b.mao@columbia.edu>
2021-05-29 12:00:00 -0700

    Version 1.4.20210531.0
    - Updated Hareru's and Hitona's latest song lives.
    - Updated Hareru's and Hitona's song lives cutouts up to 2020/05.
    - Updated Bilibili song live playlist pages.

--- js/shared.mjs
+++ js/shared.mjs
@@ -202,5 +202,5 @@ export const HANAYORI_MEMBER_IDS = ['kano', 'nonono', 'hareru', 'hitona'];
  */
 export const BILIBILI_PLAYLIST_PAGES = new Map([
   ['BV1jE411X7hH', 83],  // Hareru
-  ['BV1PE411R7w2', 51]   // Hitona
+  ['BV1PE411R7w2', 52]   // Hitona
 ]);
